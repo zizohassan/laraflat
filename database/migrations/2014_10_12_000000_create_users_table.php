3e4a6fd618a732ec89664b1e1bd7077a052abee4
Mohamedouff <mohamed@dawod-ouff.com>
2017-06-09 16:46:09 +0300

    add username in users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,6 +16,7 @@ class CreateUsersTable extends Migration
             Schema::create('users', function (Blueprint $table) {
                 $table->increments('id');
                 $table->string('name');
+                $table->string('username');
                 $table->string('email')->unique();
                 $table->string('password');
                 $table->integer('group_id');
