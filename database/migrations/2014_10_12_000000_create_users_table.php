d58c879d13657e407b8f54c982426317a1e3c659
Abdel Aziz Hassan <zizo199988@gmail.com>
2017-06-14 14:29:29 +0200

    Update 2014_10_12_000000_create_users_table.php

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,7 @@ class CreateUsersTable extends Migration
             Schema::create('users', function (Blueprint $table) {
                 $table->increments('id');
                 $table->string('name');
-                $table->string('username');
+                $table->string('username')->unique();
                 $table->string('email')->unique();
                 $table->string('password');
                 $table->integer('group_id');
